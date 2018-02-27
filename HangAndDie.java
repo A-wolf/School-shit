69cb155864589adbca75297b0ed79460e28942a6
A-wolf <sadow.dracula@gmail.com>
2018-02-27 15:04:51 +0100

    more
    more

--- HangAndDie.java
+++ HangAndDie.java
@@ -1,12 +1,17 @@
 package uppgifter;
 
+import java.util.Scanner;
+
 public class HangAndDie {
 
 	public static void main(String[] args) {
 
-		
-		
+		Scanner letter = new Scanner(System.in);
 
 	}
-	
+
+	public HangAndDie() {
+
+	}
+
 }
